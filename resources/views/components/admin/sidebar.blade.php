46d800c43bc01b60c6d118ca7f41e7162e7f2bdf
Murad <agamedov94@mail.ru>
2025-01-17 14:36:48 +0400

    commit

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -16,124 +16,17 @@
         <ul class="metismenu" id="sidenav">
             <li>
                 <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="material-icons-outlined">home</i>
-                    </div>
-                    <div class="menu-title">Dashboard</div>
+                    <div class="parent-icon"><i class="material-icons-outlined">g_translate</i></div>
+
+                    <div class="menu-title">Языки и словарь</div>
                 </a>
                 <ul>
-                    <li><a href="{{asset('assets/admin/index.html')}}"><i class="material-icons-outlined">arrow_right</i>Analysis</a>
+                    <li><a href="{{route("admin.languages.index")}}"><i class="material-icons-outlined">arrow_right</i>Языки</a>
                     </li>
-                    <li><a href="{{asset('assets/admin/index2.html')}}"><i class="material-icons-outlined">arrow_right</i>eCommerce</a>
-                    </li>
+
                 </ul>
-            </li>
-            <li>
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="material-icons-outlined">widgets</i>
-                    </div>
-                    <div class="menu-title">Widgets</div>
-                </a>
-                <ul>
-                    <li><a href="{{asset('assets/admin/widgets-data.html')}}"><i class="material-icons-outlined">arrow_right</i>Data</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/widgets-static.html')}}"><i class="material-icons-outlined">arrow_right</i>Static</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow" href="javascript:;">
-                    <div class="parent-icon"><i class="material-icons-outlined">apps</i>
-                    </div>
-                    <div class="menu-title">Apps</div>
-                </a>
-                <ul>
-                    <li><a href="{{asset('assets/admin/app-emailbox.html')}}"><i class="material-icons-outlined">arrow_right</i>Email Box</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/app-emailread.html')}}"><i class="material-icons-outlined">arrow_right</i>Email Read</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/app-chat-box.html')}}"><i class="material-icons-outlined">arrow_right</i>Chat</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/app-fullcalender.html')}}"><i class="material-icons-outlined">arrow_right</i>Calendar</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/app-to-do.html')}}"><i class="material-icons-outlined">arrow_right</i>To do</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/app-invoice.html')}}"><i class="material-icons-outlined">arrow_right</i>Invoice</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-label">UI Elements</li>
-            <li>
-                <a href="{{asset('assets/admin/cards.html')}}">
-                    <div class="parent-icon"><i class="material-icons-outlined">inventory_2</i>
-                    </div>
-                    <div class="menu-title">Cards</div>
-                </a>
             </li>
 
-            <li>
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="material-icons-outlined">shopping_bag</i>
-                    </div>
-                    <div class="menu-title">eCommerce</div>
-                </a>
-                <ul>
-                    <li><a href="{{asset('assets/admin/ecommerce-add-product.html')}}"><i class="material-icons-outlined">arrow_right</i>Add Product</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/ecommerce-products.html')}}"><i class="material-icons-outlined">arrow_right</i>Products</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/ecommerce-customers.html')}}"><i class="material-icons-outlined">arrow_right</i>Customers</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/ecommerce-customer-details.html')}}"><i class="material-icons-outlined">arrow_right</i>Customer Details</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/ecommerce-orders.html')}}"><i class="material-icons-outlined">arrow_right</i>Orders</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/ecommerce-order-details.html')}}"><i class="material-icons-outlined">arrow_right</i>Order Details</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow" href="javascript:;">
-                    <div class="parent-icon"><i class="material-icons-outlined">card_giftcard</i>
-                    </div>
-                    <div class="menu-title">Components</div>
-                </a>
-                <ul>
-                    <li><a href="{{asset('assets/admin/component-alerts.html')}}"><i class="material-icons-outlined">arrow_right</i>Alerts</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-accordions.html')}}"><i class="material-icons-outlined">arrow_right</i>Accordions</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-badges.html')}}"><i class="material-icons-outlined">arrow_right</i>Badges</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-buttons.html')}}"><i class="material-icons-outlined">arrow_right</i>Buttons</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-carousels.html')}}"><i class="material-icons-outlined">arrow_right</i>Carousels</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-media-object.html')}}"><i class="material-icons-outlined">arrow_right</i>Media Objects</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-modals.html')}}"><i class="material-icons-outlined">arrow_right</i>Modals</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-navs-tabs.html')}}"><i class="material-icons-outlined">arrow_right</i>Navs & Tabs</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-navbar.html')}}"><i class="material-icons-outlined">arrow_right</i>Navbar</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-paginations.html')}}"><i class="material-icons-outlined">arrow_right</i>Pagination</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-popovers-tooltips.html')}}"><i class="material-icons-outlined">arrow_right</i>Popovers & Tooltips</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-progress-bars.html')}}"><i class="material-icons-outlined">arrow_right</i>Progress</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-spinners.html')}}"><i class="material-icons-outlined">arrow_right</i>Spinners</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-notifications.html')}}"><i class="material-icons-outlined">arrow_right</i>Notifications</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-avtars-chips.html')}}"><i class="material-icons-outlined">arrow_right</i>Avatrs & Chips</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-typography.html')}}"><i class="material-icons-outlined">arrow_right</i>Typography</a>
-                    </li>
-                    <li><a href="{{asset('assets/admin/component-text-utilities.html')}}"><i class="material-icons-outlined">arrow_right</i>Utilities</a>
-                    </li>
-                </ul>
-            </li>
         </ul>
         <!--end navigation-->
     </div>
