d75768611dc172b33cb16ba984cb0c8d8ec6dd16
Murad <agamedov94@mail.ru>
2025-01-23 01:20:54 +0400

    commit

--- routes/mvc/views.php
+++ routes/mvc/views.php
@@ -25,9 +25,7 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
     Route::group(['prefix' => '', 'as' => 'view.'], function () {
 
         // General Pages
-        Route::get('/', function () {
-            return redirect(LaravelLocalization::getLocalizedURL(LaravelLocalization::getDefaultLocale()));
-        });
+        Route::get('/', [HomeController::class, 'index'])->name('home');
         Route::get(__('contact'), [ContactController::class, 'index'])->name('contact');
         Route::get(__('wish_list'), [WishListController::class, 'index'])->name('wishlist');
         Route::get(__('about'), [\App\Http\Controllers\Views\AboutController::class, 'index'])->name('about');
