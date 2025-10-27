b4274f7012801845eaf648edce0e8e88d9707705
Murad Agamedov <agamedov94@mail.ru>
2025-10-27 14:32:42 +0400

    commit-fix

--- app/Http/Controllers/Views/ContactController.php
+++ app/Http/Controllers/Views/ContactController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Views;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('view.pages.contact');
+        $banner = Banner::where('keyword', 'contact')->where('status', 1)->orderBy('order')->first();
+        return view('view.pages.contact', compact('banner'));
     }
 }
