26c8560d605f5574033ff114add857f8ad805ee3
Murad Agamedov <agamedov94@mail.ru>
2025-10-27 12:41:29 +0400

    commit-fix

--- resources/views/view/layouts/includes/partials/_info_center.blade.php
+++ resources/views/view/layouts/includes/partials/_info_center.blade.php
@@ -1,6 +1,9 @@
-<div class="box_style_4">
+{{--
+    <div class="box_style_4">
     <i class="icon_set_1_icon-90"></i>
     <h4><span>Book</span> by phone</h4>
     <a href="tel:{{ preg_replace('/[\s-]/', '', $companyInfo->phone) }}" class="phone">{{ $companyInfo->phone }}</a>
     <small>{{__("Monday to Friday 9.00am - 7.30pm")}}</small>
 </div>
+
+    --}}
